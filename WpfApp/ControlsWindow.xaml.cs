a3b7330f8aa820db7c1e448d02f0d2bad9fe24ed
Paweł Łęczycki <pawel@msd.info.pl>
2023-06-28 15:00:06 +0200

    Obsługa zdarzeń

--- WpfApp/ControlsWindow.xaml.cs
+++ WpfApp/ControlsWindow.xaml.cs
@@ -28,5 +28,32 @@ namespace WpfApp
             MyTextBox.Focus();
             MyTextBox.CaretIndex = MyTextBox.Text.Length;
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Shoud I disappear?", "ButtonClick", MessageBoxButton.YesNo, MessageBoxImage.Question);
+        
+            if(result == MessageBoxResult.Yes && sender is Button button)
+            {
+                button.Visibility = Visibility.Hidden;
+
+                Task.Delay(5000).ContinueWith(x => Application.Current.Dispatcher.Invoke(() => button.Visibility = Visibility.Visible));
+            }
+        }
+
+        private void CheckBox_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            if (CheckBox_V1.IsChecked == true && CheckBox_V2.IsChecked == true && CheckBox_V3.IsChecked == true)
+                CheckBox_Main.IsChecked = true;
+            else if (CheckBox_V1.IsChecked == false && CheckBox_V2.IsChecked == false && CheckBox_V3.IsChecked == false)
+                CheckBox_Main.IsChecked = false;
+            else
+                CheckBox_Main.IsChecked = null;
+        }
+
+        private void CheckBox_Main_Click(object sender, RoutedEventArgs e)
+        {
+            CheckBox_V1.IsChecked = CheckBox_V2.IsChecked = CheckBox_V3.IsChecked = CheckBox_Main.IsChecked ?? false;
+        }
     }
 }
