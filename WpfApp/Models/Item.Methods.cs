dacf5b94d777cfac008536df8b7d37a78a830c5a
Paweł Łęczycki <pawel@msd.info.pl>
2023-06-28 10:17:00 +0200

    WPF

---
+++ WpfApp/Models/Item.Methods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Models
+{
+    public partial class Item
+    {
+        public void DoSth()
+        {
+
+        }
+    }
+}
---
+++ WpfApp/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Models
+{
+    public partial class Item
+    {
+        public string Name { get; set; }
+
+    }
+}
