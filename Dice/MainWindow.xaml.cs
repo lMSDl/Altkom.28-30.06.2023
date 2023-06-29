6f96a7e8ec5138ae6ead73d1e4373600f3e1ee63
Paweł Łęczycki <pawel@msd.info.pl>
2023-06-29 14:25:14 +0200

    INotifyPropertyChanged & ObservableCollection

---
+++ Dice/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Dice.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public ObservableCollection<DiceItem> Dices { get; set; }
+        public int NumberOfDices => Dices.Count;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Dices = new ObservableCollection<DiceItem>();
+            for (int i = 0; i < 6; i++)
+            {
+                Dices.Add(new DiceItem() { Value = i + 1 });
+            }
+
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void Button_Add(object sender, RoutedEventArgs e)
+        {
+            Dices.Add(new DiceItem());
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(NumberOfDices)));
+        }
+        private void Button_Remove(object sender, RoutedEventArgs e)
+        {
+            Dices.Remove(Dices.Last());
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(NumberOfDices)));
+        }
+    }
+}
---
+++ Dice/Models/DiceItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice.Models
+{
+    public class DiceItem
+    {
+        public int Value { get; set; }
+    }
+}
