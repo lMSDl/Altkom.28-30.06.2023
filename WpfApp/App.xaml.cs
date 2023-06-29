f5c2f0604e2c472e421d8d62870f058e9a60b65e
Paweł Łęczycki <pawel@msd.info.pl>
2023-06-29 12:00:43 +0200

    Lokalizacja

--- WpfApp/App.xaml.cs
+++ WpfApp/App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using WpfApp.Models;
@@ -14,6 +16,10 @@ namespace WpfApp
     /// </summary>
     public partial class App : Application
     {
-
+        public App()
+        {
+            //Wymuszenie lokalizacji aplikacji
+            //Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("en-US");
+        }
     }
 }
